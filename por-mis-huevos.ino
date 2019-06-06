8ee85db0d534f3a44066b934cbe25b588183f5ae
Mario Galan <mario.galan@gmail.com>
2019-06-06 11:01:47 +0200

    Added data structures and code cleanup

--- por-mis-huevos.ino
+++ por-mis-huevos.ino
@@ -1,6 +1,12 @@
 #include <DHT_U.h>
 #include <DHT.h>
 
+
+/*
+ * 
+
+int incubationTemps[21] = {37.7, 37.7, 37.7, 37.7, 37.7};
+
 int SENSOR = 2;
 #define RELAY 3
 #define SENSOR2 4
@@ -12,25 +18,67 @@ float HUMEDAD;
 DHT dht(SENSOR, DHT22);
 //DHT dht2(SENSOR2, DHT11);
 
+class Day {
+  int temperature;
+  int humidity;
+  int swings;
+};
+*/
+
+class IncubatorHardware {
+  public:
+    void setTemp() {}
+    void getTemp() {}
+    void setHumidity() {}
+    void getHumidity() {}
+};
+
+class IncubationProgramme {
+};
+
+class Incubator {
+  private:
+    double objectiveTemp;
+    double actualTemp;
+    int objectiveHumidity;
+    int actualHumidity;
+    IncubatorHardware ih;
+    IncubationProgramme ip;
+    
+  public:
+    Incubator(IncubatorHardware ih, IncubationProgramme ip) {
+      this->ih = ih;
+      this->ip = ip;
+    }
+    
+    void getInfo() {}
+};
+
+
+
+
+IncubationProgramme ip;
+IncubatorHardware ih;
+Incubator incu(ih, ip);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  dht.begin();
+  //dht.begin();
   //dht2.begin();
 
-  pinMode(RELAY, OUTPUT);
+  //pinMode(RELAY, OUTPUT);
 }
 
 void loop() {
-
+/*
   t1 = dht.readTemperature();
 
   h1 = dht.readHumidity();
-  /*
+  
     t2 = dht2.readTemperature();
     h2 = dht2.readHumidity();
-  */
+  
 
   Serial.print("T1: ");
   Serial.print(t1);
@@ -58,5 +106,5 @@ void loop() {
 
   delay(1000);
 
-
+*/
 }
